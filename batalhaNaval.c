b09f90d88fd53a5c799db8e998fa7dc7ba59974f
Sehtz <sssergiosilva745@gmail.com>
2026-05-30 22:09:21 -0300

    BATALHA NAVAL - DESAFIO NÍVEL AVENTUREIRO

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -4,36 +4,39 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
 // Siga os comentários para implementar cada parte do desafio.
 
+#define LINHAS 10
+#define COLUNAS 10
+
 int main()
 {
     // Nível Novato - Posicionamento dos Navios
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
 
-    int tabuleiro[10][10] = {0};         // Inicializa o tabuleiro com 0 (vazio)
-    char linha = 'A';                    // Variável para representar as linhas do tabuleiro
-    int coluna = 0, i = 0, j = 0; // Variável para representar as colunas do tabuleiro
+    int tabuleiro[LINHAS][COLUNAS] = {0}; // Inicializa o tabuleiro com 0 (vazio)
+    char X = 'A';                         // Variável para representar as linhas do tabuleiro
+    int Y = 1, i = 0, j = 0;              // Variável para representar as colunas do tabuleiro
 
     printf("\n");
     printf("*** TABULEIRO BATALHA NAVAL ***\n\n");
     printf("    1 2 3 4 5 6 7 8 9 10\n");
     printf("   ---------------------\n");
-    for (i = 0; (i < 10) && (linha <= 'J'); i++, linha++)
+    for (i = 0; (i < LINHAS) && (X <= 'J'); i++, X++)
     {
-        printf("%c | ", linha);
-        for (j = 0; j < 10; j++)
+        printf("%c | ", X);
+        for (j = 0; j < COLUNAS; j++)
         {
             printf("%d ", tabuleiro[i][j]);
         }
         printf("\n");
     }
     printf("\n");
+    printf("==========================================================================\n");
 
     //===================================================
     // Área para Posicionar Navios
 
-    char letraNavio1, letraNavio2;            // Variável para armazenar a letra da linha do navio
-    int numeroNavio1, numeroNavio2; // Variável para armazenar o número da coluna do navio
-    
+    char linhaH, linhaV, linhaD1, linhaD2;    // Variável para armazenar a letra da linha do navio
+    int ColunaH, ColunaV, ColunaD1, ColunaD2; // Variável para armazenar o número da coluna do navio
 
     printf("\n");
     printf("Posicione seu 1° Navio na Horizontal\n\n");
@@ -43,50 +46,51 @@ int main()
     printf("1º Passo - Para posicionar o seu navio horizontal escolha uma linha entre A a J.\n");
     printf("   OBS: Apenas serão aceitos caracteres MAIÚSCULOS.\n");
     printf("Digite a linha para posicionar o navio: ");
-    scanf(" %c", &letraNavio1);                          // Lê a letra para a linha do navio
+    scanf(" %c", &linhaH); // Lê a letra para a linha do navio
     printf("\n");
-    printf("2º Passo - Escolha uma casa entre as colunas 01 a 08 para posicionar a primeira parte do navio,\n");
+    printf("2º Passo - Escolha uma casa entre as colunas 01 a 08 para posicionar a primeira part do navio,\n");
     printf("   OBS: A segunda e terceira partes do navio serão posicionadas automaticamente a direita da primeira posição.\n");
     printf("AVISO! - Seu navio será identificado com o caractere (3) no tabuleiro.\n");
     printf("Digite a coluna para posicionar o navio: "); // Lê o número para a coluna do navio
-    scanf("%d", &numeroNavio1);
+    scanf("%d", &ColunaH);
     printf("\n\n");
+    printf("==========================================================================\n");
 
+    int indexlinhaNavio1 = linhaH - 'A', indexColunaNavio1 = ColunaH - 1; // Converte a letra para um índice numérico (0 para A, 1 para B, etc.)
 
-    
-
-    int indexlinhaNavio1 = letraNavio1 - 'A', indexColunaNavio1 = numeroNavio1 - 1; // Converte a letra para um índice numérico (0 para A, 1 para B, etc.)
-
-    tabuleiro[indexlinhaNavio1][indexColunaNavio1] = 3; // Posiciona o navio no tabuleiro (ajusta a coluna para índice)
-    tabuleiro[indexlinhaNavio1][indexColunaNavio1 + 1] = 3;     // Posiciona a segunda parte do navio na horizontal
+    tabuleiro[indexlinhaNavio1][indexColunaNavio1] = 3;     // Posiciona o navio no tabuleiro (ajusta a coluna para índice)
+    tabuleiro[indexlinhaNavio1][indexColunaNavio1 + 1] = 3; // Posiciona a segunda parte do navio na horizontal
     tabuleiro[indexlinhaNavio1][indexColunaNavio1 + 2] = 3; // Posiciona a terceira parte do navio na horizontal
 
-    if ((indexlinhaNavio1 >= 0 && indexlinhaNavio1 <= 9) && (indexColunaNavio1 >= 1 && indexColunaNavio1 <= 8))
+    if ((indexlinhaNavio1 >= 0 && indexlinhaNavio1 <= 9) && (indexColunaNavio1 >= 1 && indexColunaNavio1 <= 7))
     {
         printf("\n");
         printf("*** TABULEIRO BATALHA NAVAL ***\n\n");
         printf("    1 2 3 4 5 6 7 8 9 10\n");
         printf("   ---------------------\n");
-        linha = 'A'; // Reinicia a variável para exibir o tabuleiro
-        for (i = 0; i < 10 && linha <= 'J'; i++, linha++)
+        X = 'A'; // Reinicia a variável para exibir o tabuleiro
+        for (i = 0; i < LINHAS && X <= 'J'; i++, X++)
         {
-            printf("%c | ", linha);
-            for (j = 0; j < 10; j++)
+            printf("%c | ", X);
+            for (j = 0; j < COLUNAS; j++)
             {
                 printf("%d ", tabuleiro[i][j]);
             }
             printf("\n");
         }
         printf("\n\n");
+        printf("==========================================================================\n");
     }
     else
     {
 
         printf("                  ATENÇÃO: Posicionamento inválido!\n");
-        printf("Certifique-se de que a linha está entre A e J e a coluna entre 1 e 8.\n");
-        printf("   Certifique-se de que não ultrapasse os limites do tabuleiro.\n\n");
+        printf("Por favor, verifique as instruções de posicionamento e tente novamente.\n");
+        printf("==========================================================================\n");
+        return 1;
     }
 
+    // POSICIONAMENTO DO SEGUNDO NAVIO
     printf("\n");
     printf("Posicione seu 2° Navio na Vertical\n\n");
 
@@ -95,46 +99,59 @@ int main()
     printf("1º Passo - Para posicionar o seu navio vertical Escolha uma LINHA entre A a H.\n");
     printf("   OBS: Apenas serão aceitos caracteres MAIÚSCULOS.\n");
     printf("Digite a linha para posicionar o navio: ");
-    scanf(" %c", &letraNavio2);                          // Lê a letra para a linha do navio
+    scanf(" %c", &linhaV); // Lê a letra para a linha do navio
     printf("\n");
     printf("2º Passo - Escolha uma Coluna entre 01 e 10 para posicionar a primeira parte do navio.\n");
     printf("   OBS: A segunda e terceira partes do navio serão posicionadas automaticamente a baixo da primeira posição.\n");
-    printf("AVISO! - Seu navio será identificado com o caractere (3) no tabuleiro.\n");    
+    printf("AVISO! - Seu navio será identificado com o caractere (3) no tabuleiro.\n");
     printf("Digite a coluna para posicionar o navio: "); // Lê o número para a coluna do navio
-    scanf("%d", &numeroNavio2);
+    scanf("%d", &ColunaV);
     printf("\n\n");
+    printf("==========================================================================\n");
 
-    int indexlinhaNavio2 = letraNavio2 - 'A', indexColunaNavio2 = numeroNavio2 - 1; // Converte a letra para um índice numérico (0 para A, 1 para B, etc.)
-
-    tabuleiro[indexlinhaNavio2][indexColunaNavio2] = 3;     // Posiciona o navio no tabuleiro (ajusta a coluna para índice)
-    tabuleiro[indexlinhaNavio2 + 1][indexColunaNavio2] = 3; // Posiciona a segunda parte do navio na horizontal
-    tabuleiro[indexlinhaNavio2 + 2][indexColunaNavio2] = 3; // Posiciona a terceira parte do navio na horizontal
-
-    if ((indexlinhaNavio2 >= 1 && indexlinhaNavio2 <= 8) && (indexColunaNavio2 >= 0 && indexColunaNavio2 <= 9))
+    if (linhaV > 'H' && ColunaV > 10 && ColunaV < 1)
     {
+        printf("                  ATENÇÃO: Posicionamento inválido!\n");
+        printf("Por favor, verifique as instruções de posicionamento e tente novamente.\n");
+        printf("==========================================================================\n");
+        return 1;
+    }
+
+    int indexlinhaNavio2 = linhaV - 'A', indexColunaNavio2 = ColunaV - 1; // Converte a letra para um índice numérico (0 para A, 1 para B, etc.)
+
+    if (tabuleiro[indexlinhaNavio2][indexColunaNavio2] == 0 &&     // Posiciona o navio no tabuleiro (ajusta a coluna para índice)
+        tabuleiro[indexlinhaNavio2 + 1][indexColunaNavio2] == 0 && // Posiciona a segunda parte do navio na horizontal
+        tabuleiro[indexlinhaNavio2 + 2][indexColunaNavio2] == 0)
+    {
+        tabuleiro[indexlinhaNavio2][indexColunaNavio2] = 3;     // Posiciona o navio no tabuleiro (ajusta a coluna para índice)
+        tabuleiro[indexlinhaNavio2 + 1][indexColunaNavio2] = 3; // Posiciona a segunda parte do navio na horizontal
+        tabuleiro[indexlinhaNavio2 + 2][indexColunaNavio2] = 3; // Posiciona a terceira parte do navio na horizontal
+
         printf("\n");
         printf("*** TABULEIRO BATALHA NAVAL ***\n\n");
         printf("    1 2 3 4 5 6 7 8 9 10\n");
         printf("   ---------------------\n");
-        linha = 'A'; // Reinicia a variável para exibir o tabuleiro
-        for (i = 0; i < 10 && linha <= 'J'; i++, linha++)
+        X = 'A'; // Reinicia a variável para exibir o tabuleiro
+        for (i = 0; i < LINHAS && X <= 'J'; i++, X++)
         {
-            printf("%c | ", linha);
-            for (j = 0; j < 10; j++)
+            printf("%c | ", X);
+            for (j = 0; j < COLUNAS; j++)
             {
                 printf("%d ", tabuleiro[i][j]);
             }
             printf("\n");
         }
         printf("\n\n");
+        printf("==========================================================================\n");
     }
     else
     {
-
         printf("                  ATENÇÃO: Posicionamento inválido!\n");
-        printf("Certifique-se de que a linha está entre A e H e a coluna entre 1 e 10.\n");
-        printf("   Certifique-se de que não ultrapasse os limites do tabuleiro.\n\n");
+        printf("Por favor, verifique as instruções de posicionamento e tente novamente.\n");
+        printf("==========================================================================\n");
+        return 1;
     }
+    printf("\n\n");
 
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
@@ -143,6 +160,139 @@ int main()
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
     // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
     // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+
+    // POSICIONAMENTO DO TERCEIRO NAVIO DIAGONAL (DA ESQUERDA PARA A DIREITA)
+    printf("\n");
+    printf("Posicione seu 3° Navio na Diagonal (DA ESQUERDA PARA A DIREITA)\n\n");
+
+    printf("***Instruções de Posicionamento***\n\n");
+
+    printf("1º Passo - Para posicionar o seu navio na Diagonal Escolha uma LINHA entre A a H.\n");
+    printf("   OBS: Apenas serão aceitos caracteres MAIÚSCULOS.\n");
+    printf("Digite a linha para posicionar o navio: ");
+    scanf(" %c", &linhaD1); // Lê a letra para a linha do navio
+    printf("\n");
+    printf("2º Passo - Escolha uma Coluna entre 01 e 08 para posicionar a primeira parte do navio.\n");
+    printf("   OBS: A segunda e terceira partes do navio serão posicionadas automaticamente a baixo e direita da primeira posição.\n");
+    printf("AVISO! - Seu navio será identificado com o caractere (3) no tabuleiro.\n");
+    printf("Digite a coluna para posicionar o navio: "); // Lê o número para a coluna do navio
+    scanf("%d", &ColunaD1);
+    printf("\n\n");
+    printf("==========================================================================\n");
+
+    if (linhaD1 > 'H' && ColunaD1 > 8 && ColunaD1 < 1)
+    {
+        printf("                  ATENÇÃO: Posicionamento inválido!\n");
+        printf("Por favor, verifique as instruções de posicionamento e tente novamente.\n");
+        printf("==========================================================================\n");
+        return 1;
+    }
+    else
+    {
+
+        int indexlinhaNavio3 = linhaD1 - 'A', indexColunaNavio3 = ColunaD1 - 1; // Converte a letra para um índice numérico (0 para A, 1 para B, etc.)
+
+        if (tabuleiro[indexlinhaNavio3][indexColunaNavio3] == 0 &&         // Posiciona o navio no tabuleiro (ajusta a coluna para índice)
+            tabuleiro[indexlinhaNavio3 + 1][indexColunaNavio3 + 1] == 0 && // Posiciona a segunda parte do navio na diagonal
+            tabuleiro[indexlinhaNavio3 + 2][indexColunaNavio3 + 2] == 0)
+        {
+            tabuleiro[indexlinhaNavio3][indexColunaNavio3] = 3;         // Posiciona o navio no tabuleiro (ajusta a coluna para índice)
+            tabuleiro[indexlinhaNavio3 + 1][indexColunaNavio3 + 1] = 3; // Posiciona a segunda parte do navio na diagonal
+            tabuleiro[indexlinhaNavio3 + 2][indexColunaNavio3 + 2] = 3; // Posiciona a terceira parte do navio na diagonal
+
+            printf("\n");
+            printf("*** TABULEIRO BATALHA NAVAL ***\n\n");
+            printf("    1 2 3 4 5 6 7 8 9 10\n");
+            printf("   ---------------------\n");
+            X = 'A'; // Reinicia a variável para exibir o tabuleiro
+            for (i = 0; i < LINHAS && X <= 'J'; i++, X++)
+            {
+                printf("%c | ", X);
+                for (j = 0; j < COLUNAS; j++)
+                {
+                    printf("%d ", tabuleiro[i][j]);
+                }
+                printf("\n");
+            }
+            printf("\n\n");
+            printf("==========================================================================\n");
+        }
+        else
+        {
+            printf("                  ATENÇÃO: Posicionamento inválido!\n");
+            printf("Por favor, verifique as instruções de posicionamento e tente novamente.\n");
+            printf("==========================================================================\n");
+            return 1;
+        }
+        printf("\n\n");
+    }
+
+    // POSICIONAMENTO DO QUARTO NAVIO DIAGONAL (DA DIREITA PARA A ESQUERDA)
+
+    printf("\n");
+    printf("Posicione seu 4° Navio na Diagonal (DA DIREITA PARA A ESQUERDA)\n\n");
+
+    printf("***Instruções de Posicionamento***\n\n");
+
+    printf("1º Passo - Para posicionar o seu navio na Diagonal Escolha uma LINHA entre A a H.\n");
+    printf("   OBS: Apenas serão aceitos caracteres MAIÚSCULOS.\n");
+    printf("Digite a linha para posicionar o navio: ");
+    scanf(" %c", &linhaD2); // Lê a letra para a linha do navio
+    printf("\n");
+    printf("2º Passo - Escolha uma Coluna entre 03 e 10 para posicionar a primeira parte do navio.\n");
+    printf("   OBS: A segunda e terceira partes do navio serão posicionadas automaticamente ABAIXO e ESQUERDA da primeira posição.\n");
+    printf("AVISO! - Seu navio será identificado com o caractere (3) no tabuleiro.\n");
+    printf("Digite a coluna para posicionar o navio: "); // Lê o número para a coluna do navio
+    scanf("%d", &ColunaD2);
+    printf("\n\n");
+    printf("==========================================================================\n");
+
+    if (linhaD2 > 'H' && ColunaD2 > 10 && ColunaD2 < 3)
+    {
+        printf("                  ATENÇÃO: Posicionamento inválido!\n");
+        printf("Por favor, verifique as instruções de posicionamento e tente novamente.\n");
+        printf("==========================================================================\n");
+        return 1;
+    }
+    else
+    {
+
+        int indexlinhaNavio4 = linhaD2 - 'A', indexColunaNavio4 = ColunaD2 - 1; // Converte a letra para um índice numérico (0 para A, 1 para B, etc.)
+
+        if (tabuleiro[indexlinhaNavio4][indexColunaNavio4] == 0 &&         // Posiciona o navio no tabuleiro (ajusta a coluna para índice)
+            tabuleiro[indexlinhaNavio4 + 1][indexColunaNavio4 - 1] == 0 && // Posiciona a segunda parte do navio na diagonal
+            tabuleiro[indexlinhaNavio4 + 2][indexColunaNavio4 - 2] == 0)
+        {
+            tabuleiro[indexlinhaNavio4][indexColunaNavio4] = 3;         // Posiciona o navio no tabuleiro (ajusta a coluna para índice)
+            tabuleiro[indexlinhaNavio4 + 1][indexColunaNavio4 - 1] = 3; // Posiciona a segunda parte do navio na diagonal
+            tabuleiro[indexlinhaNavio4 + 2][indexColunaNavio4 - 2] = 3; // Posiciona a terceira parte do navio na diagonal
+
+            printf("\n");
+            printf("*** TABULEIRO BATALHA NAVAL ***\n\n");
+            printf("    1 2 3 4 5 6 7 8 9 10\n");
+            printf("   ---------------------\n");
+            X = 'A'; // Reinicia a variável para exibir o tabuleiro
+            for (i = 0; i < LINHAS && X <= 'J'; i++, X++)
+            {
+                printf("%c | ", X);
+                for (j = 0; j < COLUNAS; j++)
+                {
+                    printf("%d ", tabuleiro[i][j]);
+                }
+                printf("\n");
+            }
+            printf("\n\n");
+            printf("==========================================================================\n");
+        }
+        else
+        {
+            printf("                  ATENÇÃO: Posicionamento inválido!\n");
+            printf("Por favor, verifique as instruções de posicionamento e tente novamente.\n");
+            printf("==========================================================================\n");
+            return 1;
+        }
+        printf("\n\n");
+    }
 
     // Nível Mestre - Habilidades Especiais com Matrizes
     // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
